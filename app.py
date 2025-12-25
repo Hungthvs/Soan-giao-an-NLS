aeb4fde14bdb053ea2c351261157766f0566c061
Hungthvs <15@minhhieu.org>
2025-12-25 16:13:16 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -199,8 +199,7 @@ if "GEMINI_API_KEY" in st.secrets:
     api_key = st.secrets["GEMINI_API_KEY"]
 else:
     with st.sidebar:
-        st.header("🔐 AIzaSyALw3LxREx0e7Hv9cDs8TEI6U2GZHEf6Bg
-")
+        st.header("🔐 Cấu hình")
         api_key = st.text_input("Nhập API Key:", type="password")
 
 if api_key:
